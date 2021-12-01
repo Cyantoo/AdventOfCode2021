57bd8decc8c8ffc198fc73442dae71f602a69707
Antoine Colin <antoine-28@hotmail.fr>
2021-12-01 11:19:40 +0100

    add day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            Console.WriteLine(projectDirectory);
+            string[] lines = File.ReadAllLines(projectDirectory+@"\input.txt");
+            int[] lines_ints = Array.ConvertAll(lines, s => int.Parse(s));
+            int result = 0;
+            for(int i =3; i<lines_ints.Length; i++)
+            {
+                if(lines_ints[i-2]+lines_ints[i-1]+lines_ints[i] > lines_ints[i - 3] + lines_ints[i - 2] + lines_ints[i-1])
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
