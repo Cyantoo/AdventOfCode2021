45c36d7507df170f1058a48a3d6c4a6c94b40cd1
Antoine Colin <antoine-28@hotmail.fr>
2021-12-05 10:54:28 +0100

    Add day4

---
+++ Day4/BingoBoard.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace Day4
+{
+    class BingoBoard
+    {
+        private int[,] board;
+        private bool[,] marks;
+        public BingoBoard(string[] string_board)
+        {
+            if(string_board.Length != 5)
+            {
+                Console.WriteLine("Wrong number of lines when creating BingoBoard");
+                return;
+            }
+            board = new int[5, 5];
+            for(int i = 0; i< 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    string number = string.Join("", string_board[i].Skip(3 * j).Take(2));
+                    board[i, j] = int.Parse(number);
+                }
+
+            }
+            marks = new bool[5, 5]; // initializes to false
+            
+
+        }
+
+        public bool ProcessNumber(int chosen_number) // returns true if the bingo is now solved
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if(board[i,j] == chosen_number)
+                    {
+                        marks[i, j] = true;
+                    }
+                }
+            }
+            return CheckForBingo();
+        }
+
+        public int SumRemainingNumbers()
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (!marks[i, j]) sum += board[i, j];
+                }
+            }
+            return sum;
+        }
+
+        public bool CheckForBingo()
+        {
+            bool result = false;
+            // have to check 5 lines, 5 columns
+            for(int i = 0; i<5; i++)
+            {
+                bool col_all_true = true;
+                bool row_all_true = true;
+                for(int j =0; j<5; j++)
+                {
+                    if (!marks[i, j]) row_all_true = false;
+                    if (!marks[j, i]) col_all_true = false;
+
+                }
+                if (col_all_true || row_all_true)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse Input
+            string[] lines = InputReader.ReadLines("input.txt");
+            int[] chosen_numbers = Array.ConvertAll(lines[0].Split(','), s => int.Parse(s));
+            // Parse bingo boards
+            List<BingoBoard> bingoBoards = new();
+            for(int i = 2; i<lines.Length;  i+=6 )
+            {
+                bingoBoards.Add(new BingoBoard(lines.Skip(i).Take(5).ToArray()));
+            }
+            foreach (int chosen_number in chosen_numbers)
+            {
+                foreach(BingoBoard bingoBoard in new List<BingoBoard>(bingoBoards))
+                {
+                    if(bingoBoard.ProcessNumber(chosen_number))
+                    {
+                        bingoBoards.Remove(bingoBoard);
+                    }
+                    if(bingoBoards.Count == 0)
+                    {
+                        Console.WriteLine(bingoBoard.SumRemainingNumbers() * chosen_number);
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
+/* Part 1
+ *        static void Main(string[] args)
+        {
+            // Parse Input
+            string[] lines = InputReader.ReadLines("input.txt");
+            int[] chosen_numbers = Array.ConvertAll(lines[0].Split(','), s => int.Parse(s));
+            // Parse bingo boards
+            List<BingoBoard> bingoBoards = new();
+            for(int i = 2; i<lines.Length;  i+=6 )
+            {
+                bingoBoards.Add(new BingoBoard(lines.Skip(i).Take(5).ToArray()));
+            }
+            foreach (int chosen_number in chosen_numbers)
+            {
+                foreach(BingoBoard bingoBoard in bingoBoards)
+                {
+                    if(bingoBoard.ProcessNumber(chosen_number))
+                    {
+                        Console.WriteLine(bingoBoard.SumRemainingNumbers() * chosen_number);
+                        return;
+                    }
+                }
+            }
+        }
+*/
